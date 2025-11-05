067ffb4747b7b266b1d886673d9648f9be0743be
AkashGB-1234 <pes1ug23am034@pesu.pes.edu>
2025-11-05 14:15:41 +0530

    chore(app): integrate cartRoutes with Express application and enable /api/cart endpoint (US-004)

---
+++ src/backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const catalogRoutes = require("./routes/catalogRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// API endpoints
+app.use("/api/catalog", catalogRoutes);
+app.use("/api/cart", cartRoutes);
+
+// Default server port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;

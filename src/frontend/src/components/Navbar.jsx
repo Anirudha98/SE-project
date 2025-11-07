89a17e202544b113e3473882045f61315eea1122
Akshay Kumar K <PES1UG23AM037@pesu.pes.edu>
2025-11-07 19:12:08 +0530

    Update Navbar.jsx

--- src/frontend/src/components/Navbar.jsx
+++ src/frontend/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { CartContext } from "../context/CartContext";
 
 const Navbar = () => {
+  const { cart } = useContext(CartContext);
+  const cartCount = cart.reduce((sum, item) => sum + item.qty, 0);
+
   return (
     <nav style={{ background: "#222", padding: "10px" }}>
       <ul
@@ -13,8 +17,21 @@ const Navbar = () => {
           color: "#fff",
         }}
       >
-        <li><Link to="/" style={{ color: "#fff", textDecoration: "none" }}>Home</Link></li>
-        <li><Link to="/products" style={{ color: "#fff", textDecoration: "none" }}>Products</Link></li>
+        <li>
+          <Link to="/" style={{ color: "#fff", textDecoration: "none" }}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/products" style={{ color: "#fff", textDecoration: "none" }}>
+            Products
+          </Link>
+        </li>
+        <li>
+          <Link to="/cart" style={{ color: "#fff", textDecoration: "none" }}>
+            🛒 Cart ({cartCount})
+          </Link>
+        </li>
       </ul>
     </nav>
   );

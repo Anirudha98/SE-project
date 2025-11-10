67cdf8e4ffa4413560a38377e2730966edbc449e
AGB2210 <agarbki3844@gmail.com>
2025-11-10 17:05:15 +0530

    Update App.js

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -49,6 +49,8 @@ function App() {
             />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<RequireAuth><Dashboard /></RequireAuth>} />
+            <Route path="/orders" element={<RequireAuth><Orders /></RequireAuth>} />
           </Routes>
         </Router>
       </CartProvider>

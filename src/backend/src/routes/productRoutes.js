7f0827b3ebaee99aeb2a8cea626d6ed50ebe177b
AGB2210 <agarbki3844@gmail.com>
2025-11-10 17:14:49 +0530

    Update productRoutes.js

--- src/backend/src/routes/productRoutes.js
+++ src/backend/src/routes/productRoutes.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const router = express.Router();
-const { getProducts, addProduct } = require('../controllers/productController');
+const express = require("express");
+const {
+  getProducts,
+  addProduct,
+  searchProducts, 
+} = require("../controllers/productController");
 
-router.get('/', getProducts);
-router.post('/', addProduct);
+const router = express.Router();
+
+router.get("/", getProducts);
+router.post("/", addProduct);
 router.get("/search", searchProducts);
 
 module.exports = router;
